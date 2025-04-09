e18271520c8f64f10c40702dec5ba2e9e71d1378
andreicismas <andrei_cismas@yahoo.it>
2025-04-09 23:49:50 +0200

    new

---
+++ script.js
@@ -0,0 +1,22 @@
+document.querySelector(".menu-toggle").addEventListener("click", function() {
+  document.querySelector(".nav-links").classList.toggle("active");
+});
+
+  const toggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+
+
+
+setTimeout(function() {
+  document.getElementById("whatsapp-btn").style.display = "block";
+}, 3000);
+
+document.getElementById("whatsapp-btn").addEventListener("click", function() {window.location.href = "https://wa.me/+393463244526";
+});
+
+
+  
