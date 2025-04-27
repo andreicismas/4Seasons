1b8ee8db0deafcd9cd821e37408457e3b7106c11
andreicismas <andrei_cismas@yahoo.it>
2025-04-27 20:36:21 +0200

    add logo

--- script.js
+++ script.js
@@ -6,14 +6,6 @@ document.querySelector(".menu-toggle").addEventListener("click", function () {
 
 
 
-setTimeout(function () {
-  document.getElementById("whatsapp-btn").style.display = "block";
-}, 3000);
-
-document.getElementById("whatsapp-btn").addEventListener("click", function () {
-  window.location.href = "https://wa.me/+393463244526";
-});
-
 
 
 
